437010afd6d5eed887562c3eb760dbd217b774c0
unknown <netsahabat08@gmail.com>
2023-10-29 00:02:32 +0800

    Build 29 Oktober 2023

---
+++ public/build/assets/GuestLayout-7122f857.js
@@ -0,0 +1,1 @@
+import{a as e}from"./app-3e06dd04.js";function t({children:s}){return e("div",{className:"flex items-center justify-center py-20 bg-white md:min-h-screen md:flex-cols",children:e("div",{className:"px-[20px] py-[28px] bg-white drop-shadow-md rounded-xl",children:s})})}export{t as G};
